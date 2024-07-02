f5785fd5232f4cbdb06f0a555dd9ce61b2b4762b
Eskil Q <eskil@queseth.se>
2024-07-02 20:30:41 +0200

    remove commented out code

--- src/model/Terrain.ts
+++ src/model/Terrain.ts
@@ -1,28 +1,10 @@
-import * as THREE from 'three';
-import { Lane, laneToOffset } from './Lane';
-import MetalTexture from '../../assets/metal.png';
+import { Lane } from './Lane';
 
 export enum TerrainType {
     Ramp,
     Wagon,
 }
 
-/*const ramp = new THREE.Group();
-
-const metalTexture = new THREE.TextureLoader().load(MetalTexture);
-
-metalTexture.wrapS = THREE.RepeatWrapping;
-metalTexture.wrapT = THREE.RepeatWrapping;
-metalTexture.repeat.set(1,1);
-
-const material = new THREE.MeshStandardMaterial({map: metalTexture, side: THREE.DoubleSide});
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1.8, Math.sqrt(10*10+4*4)), material);
-
-plane.rotateX(Math.atan(10/4));
-
-ramp.add(plane);
-*/
 export class Terrain {
     type: TerrainType;
     offset: number;
@@ -30,26 +12,10 @@ export class Terrain {
     constructor(type: TerrainType, lane: Lane, offset: number) {
         this.type = type;
         this.offset = offset;
-
-        /*switch(type) {
-            case TerrainType.Ramp:
-                this.mesh = ramp.clone(true);
-                break;
-            case TerrainType.Wagon:
-                const geometry = new THREE.BoxGeometry(1.8,4,10);
-                this.mesh = new THREE.Mesh(geometry, material);
-                break;
-        }*/
-
-        /*this.mesh.position.x = laneToOffset(lane);
-        this.mesh.position.y = 2;
-        this.mesh.position.z = offset;
-        scene.add(this.mesh);*/
     }
 
     update(delta: number, gameSpeed: number) {
         this.offset -= delta * gameSpeed;
-        //this.mesh.position.z = this.offset;
     }
 
     /// returns the interval of z coordinates that this terrain piece is valid for
@@ -66,10 +32,5 @@ export class Terrain {
             case TerrainType.Wagon:
                 return 4;
         }
-    }
-
-    remove() {
-        //this.mesh.parent?.remove(this.mesh);
-        //this.mesh.geometry.dispose();
     }
 }
