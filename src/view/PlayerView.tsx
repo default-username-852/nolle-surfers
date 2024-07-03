efe0e39733a3aa9430f80d98d109fa71460e4918
Eskil Q <eskil@queseth.se>
2024-07-03 13:52:48 +0200

    player lane switching is now smoothed

--- src/view/PlayerView.tsx
+++ src/view/PlayerView.tsx
@@ -1,10 +1,22 @@
 import Player from "../model/Player";
 import * as React from "react";
 import { laneToOffset } from "../model/Lane";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function PlayerView({player}: { player: Player }): React.JSX.Element {
+    const meshRef = React.useRef<THREE.Mesh>(null);
+    const xPos = React.useRef(0);
+
+    useFrame((s, delta) => {
+        if (meshRef.current) {
+            meshRef.current.position.x = THREE.MathUtils.damp(meshRef.current.position.x, laneToOffset(player.lane), 10, delta);
+            xPos.current = meshRef.current.position.x;
+        }
+    })
+
     return (
-        <mesh position={[laneToOffset(player.lane),player.height + 1,0]}>
+        <mesh position={[xPos.current,player.height + 1,0]} ref={meshRef}>
             <boxGeometry args={[1,2,1]}/>
             <meshStandardMaterial color={0xff0000}/>
         </mesh>
