e5e07e20f209c93b509af0d02b87fd8acb4a7bdd
Eskil Q <eskil@queseth.se>
2024-07-15 17:26:25 +0200

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,46 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|glb)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      //template: '"!!html-loader!src/index.html",',
-      title: 'Nolle-Surfers',
-      template: 'src/index.html',
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.glb'],
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  devServer: {
-    client: {
-      overlay: true,
-      progress: true,
+module.exports = 
+function foo(env, argv) {
+  return {
+    entry: './src/index.tsx',
+    devtool: argv.mode === 'development' ? 'eval-source-map' : undefined,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|glb)$/i,
+          type: 'asset/resource',
+        },
+      ],
     },
-    compress: true,
-    hot: true,
-  }
+    plugins: [
+      new HtmlWebpackPlugin({
+        //template: '"!!html-loader!src/index.html",',
+        title: 'Nolle-Surfers',
+        template: 'src/index.html',
+      }),
+    ],
+    resolve: {
+      extensions: ['.ts', '.js', '.tsx', '.glb'],
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    devServer: {
+      client: {
+        overlay: true,
+        progress: true,
+      },
+      compress: true,
+      hot: true,
+    }
+  };
 };
