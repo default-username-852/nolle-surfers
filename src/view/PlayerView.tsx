6d7071accfb265d3fab07b291dcd14c2a0b54e9a
Eskil Q <eskil@queseth.se>
2024-07-17 17:45:20 +0200

    add running turtle animation

--- src/view/PlayerView.tsx
+++ src/view/PlayerView.tsx
@@ -1,14 +1,20 @@
 import Player from "../model/Player";
 import * as React from "react";
 import { laneToOffset } from "../model/Lane";
-import { useFrame } from "@react-three/fiber";
+import { ObjectMap, useFrame, useLoader } from "@react-three/fiber";
 import * as THREE from "three";
 import { gameState } from "..";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Turtle from "./turtle_anim.glb";
 
 export default function PlayerView(): React.JSX.Element {
     const meshRef = React.useRef<THREE.Mesh>(null);
     const xPos = React.useRef(0);
-
+    const model = useLoader(GLTFLoader, Turtle) as GLTF & ObjectMap;
+    console.log(model.animations);
+    const mixer = React.useRef<THREE.AnimationMixer>(new THREE.AnimationMixer(model.scene));
+    mixer.current.clipAction(model.animations[1]).play();
+    
     useFrame((s, delta) => {
         if (meshRef.current) {
             const player = gameState.currentInstance.player;
@@ -16,12 +22,12 @@ export default function PlayerView(): React.JSX.Element {
             xPos.current = meshRef.current.position.x;
             meshRef.current.position.y = player.height;
         }
-    })
+        mixer.current.update(delta);
+    });
 
     return (
-        <mesh position={[xPos.current,1,0]} ref={meshRef}>
-            <boxGeometry args={[1,2,1]}/>
-            <meshStandardMaterial color={0xff0000}/>
-        </mesh>
+        <>
+            <primitive object={model.scene} ref={meshRef}/>
+        </>
     );
 }
