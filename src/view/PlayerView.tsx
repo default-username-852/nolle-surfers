4a8c3ff2ce4047502e3c9da04c9e32f05f12f809
Eskil Q <eskil@queseth.se>
2024-08-07 13:06:30 +0200

    add rolling animation

--- src/view/PlayerView.tsx
+++ src/view/PlayerView.tsx
@@ -12,6 +12,7 @@ Source: https://sketchfab.com/3d-models/turtle-animation-a150fca214a64c7787a4e4b
 Title: Turtle-animation
 Used with modifications
 */
+
 import Turtle from "./turtle_anim.glb";
 import { useSnapshot } from "valtio";
 import { RunningState } from "../model/Player";
@@ -23,29 +24,55 @@ export default function PlayerView(): React.JSX.Element {
     const xPos = React.useRef(0);
     const model = useLoader(GLTFLoader, Turtle) as GLTF & ObjectMap;
     const mixer = React.useRef<THREE.AnimationMixer>(new THREE.AnimationMixer(model.scene));
-    const running = React.useRef(mixer.current.clipAction(model.animations[1]));
+    const running = React.useRef(mixer.current.clipAction(model.animations[2]));
+    const rolling = React.useRef(mixer.current.clipAction(model.animations[1]));
     const falling = React.useRef(mixer.current.clipAction(model.animations[0]));
 
-    const onGround = useSnapshot(gameState).currentInstance.player.onGround;
+    const player = useSnapshot(gameState).currentInstance.player;
 
     React.useEffect(() => {
         falling.current.play();
         running.current.play();
+        rolling.current.play();
     }, []);
 
     React.useEffect(() => {
-        if (onGround) {
-            if (falling.current.isRunning()) {
-                falling.current.fadeOut(0.5);
-            }
-            running.current.reset().fadeIn(0.1);
-        } else {
-            if (running.current.isRunning()) {
-                running.current.fadeOut(0.5);
-            }
-            falling.current.reset().fadeIn(0.1);
+        switch(player.runningState) {
+            case RunningState.OnGround:
+            case RunningState.AirBuffer:
+                if(falling.current.isRunning()) {
+                    falling.current.fadeOut(0.5);
+                }
+                if(rolling.current.isRunning()) {
+                    rolling.current.fadeOut(0.2);
+                }
+                if(!running.current.isRunning()) {
+                    running.current.reset().fadeIn(0.1);
+                }
+                break;
+            case RunningState.MidAir:
+                if(running.current.isRunning()) {
+                    running.current.fadeOut(0.5);
+                }
+                if(rolling.current.isRunning()) {
+                    rolling.current.fadeOut(0.5);
+                }
+                falling.current.reset().fadeIn(0.1);
+                break;
+            case RunningState.Rolling:
+            case RunningState.AirRoll:
+                if(running.current.isRunning()) {
+                    running.current.fadeOut(0.2);
+                }
+                if(falling.current.isRunning()) {
+                    falling.current.fadeOut(0.2);
+                }
+                if(!rolling.current.isRunning()) {
+                    rolling.current.reset().fadeIn(0.1);
+                }
+                break;
         }
-    }, [onGround]);
+    }, [player.runningState]);
 
     useFrame((s, delta) => {
         const player = gameState.currentInstance.player;
@@ -56,11 +83,6 @@ export default function PlayerView(): React.JSX.Element {
             }
             xPos.current = meshRef.current.position.x;
             meshRef.current.position.y = player.height;
-            if (player.runningState === RunningState.Rolling || player.runningState === RunningState.AirRoll) {
-                meshRef.current.scale.y = 0.5;
-            } else {
-                meshRef.current.scale.y = 1;
-            }
         }
         mixer.current.update(delta);
     });
